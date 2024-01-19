3a8a598bdc9eb12d228b975fe35cccde757a84be
Fons Sabalbosa <86940332+fons-sabalbosajr@users.noreply.github.com>
2024-01-19 12:10:42 +0800

    update app.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,10 +8,10 @@ function App() {
   return(
      <BrowserRouter>
      <Routes>
-      <Route path='/' element={<Login />}></Route>
+      <Route path='/' element={<Home />}></Route>
       <Route path='/register' element={<Register />}></Route>
       <Route path='/home' element={<Home />}></Route>
-      <Route path='/login' element={<Login />}></Route> 
+      <Route path='/login' element={<Login />}></Route>
       </Routes>
       </BrowserRouter>
   )
